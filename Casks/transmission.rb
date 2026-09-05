f8750232b44a95afd6dc0189e353e1c4b86a4a5c
servitola <servitola@gmail.com>
2026-09-05 18:09:04 +0300

    Add tap with transmission cask (Liquid Glass fork build)
    Cask points at the servitola/transmission GitHub release; release script rebuilds, signs and bumps it.

---
+++ Casks/transmission.rb
@@ -0,0 +1,34 @@
+cask "transmission" do
+  version "4.2.0-dev.20260905"
+  sha256 "41857ec38cdc4963327111645404d207cfdf3aa9ec368613d78af11bf1044056"
+
+  url "https://github.com/servitola/transmission/releases/download/v#{version}/Transmission-#{version}.zip"
+  name "Transmission"
+  desc "BitTorrent client, personal fork with native Liquid Glass UI"
+  homepage "https://github.com/servitola/transmission"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :tahoe
+
+  app "Transmission.app"
+
+  # The build is signed with Developer ID but not notarized, so Gatekeeper refuses
+  # to open the quarantined copy Homebrew installs. Dropping the attribute is what
+  # `brew install --no-quarantine` would do; done here so plain `brew upgrade` works.
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Transmission.app"]
+  end
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.m0k.transmission.sfl*",
+    "~/Library/Application Support/Transmission",
+    "~/Library/Caches/org.m0k.transmission",
+    "~/Library/Preferences/org.m0k.transmission.plist",
+    "~/Library/Saved Application State/org.m0k.transmission.savedState",
+  ]
+end

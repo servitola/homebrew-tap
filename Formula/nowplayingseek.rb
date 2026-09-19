739d2296518e03cb92f6078bfcb252083bc7275f
servitola <servitola@gmail.com>
2026-09-19 10:08:23 +0300

    nowplayingseek 0.5.0

--- Formula/nowplayingseek.rb
+++ Formula/nowplayingseek.rb
@@ -1,8 +1,8 @@
 class Nowplayingseek < Formula
   desc "Seek, skip and control whatever is Now Playing on macOS"
   homepage "https://github.com/servitola/nowplayingseek"
-  url "https://github.com/servitola/nowplayingseek/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "a24fc6903ad867d01f701653a19d825bebd0416b82674ded41a6ffcd3b588cec"
+  url "https://github.com/servitola/nowplayingseek/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "710f57d9229661b4e5f79adfc45bf2ec636cb6b97d593d6d677e01f72e4685a0"
   license "AGPL-3.0-only"
   head "https://github.com/servitola/nowplayingseek.git", branch: "main"
 
